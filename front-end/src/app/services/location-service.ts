1725f2cb13dc06e50c1a797b0dd4f069bd31c1d9
Brt <barthunerbeincoding@gmail.com>
2026-01-08 16:24:30 +0100

    add locationservice to read current location for observations

---
+++ front-end/src/app/services/location-service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LocationService {
+  getCurrentLocation(): Promise<GeolocationPosition> {
+    return new Promise((resolve, reject) => {
+      if (!navigator.geolocation) {
+        reject('Geolocation is not supported by your browser');
+      }
+
+      navigator.geolocation.getCurrentPosition(
+        (position) => resolve(position),
+        (error) => reject(error),
+        { enableHighAccuracy: true }
+      );
+    });
+  }
+}
