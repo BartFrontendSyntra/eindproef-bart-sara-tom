1768bd92ff03a134f933cc10a8dd7a4d8294f339
Brt <barthunerbeincoding@gmail.com>
2026-01-08 14:31:55 +0100

    added admin login logic

--- front-end/src/app/pages/admin-login/admin-login.ts
+++ front-end/src/app/pages/admin-login/admin-login.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { AuthenticationService } from '../../services/authentication-service';
+import { form, required } from '@angular/forms/signals';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-login',
@@ -8,4 +11,43 @@ import { Component } from '@angular/core';
 })
 export class AdminLogin {
 
+  authenticationService: AuthenticationService = inject(AuthenticationService);
+  router: Router = inject(Router);
+
+  loginModel = signal<Credentials>({
+    username: '',
+    password: '',
+    requiredRole: 'admin',
+  });
+
+  loginForm = form(this.loginModel, schemaPath => {
+    required(schemaPath.username, { message: 'Username is required' });
+    required(schemaPath.password, { message: 'Password is required' });
+  });
+
+  onSubmit(event: Event) {
+    event.preventDefault();
+   
+    const credentials = this.loginModel();
+    console.log('Logging in with:', credentials);
+    // e.g., await this.authenticationService.login(credentials);
+    this.authenticationService
+      .login(credentials)
+      .then(data => {
+        console.log('Login successful, token:', data);
+        const redirectUrl =
+          sessionStorage.getItem('redirect_after_login') || '/home';
+        sessionStorage.removeItem('redirect_after_login');
+        this.router.navigate([redirectUrl]);
+      })
+      .catch(error => {
+        console.error('Login failed:', error);
+      });
+  }
+
+}
+interface Credentials {
+  username: string;
+  password: string;
+  requiredRole: string;
 }
