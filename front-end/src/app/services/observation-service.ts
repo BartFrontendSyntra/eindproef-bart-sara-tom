9ab8835258c23012950115d5b529a2873f6dc6f3
Brt <barthunerbeincoding@gmail.com>
2026-01-08 16:25:12 +0100

    add observation service to post observations to backend

---
+++ front-end/src/app/services/observation-service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ObservationService {
+    private apiUrl = 'http://localhost:8000/api/observations';
+
+
+    postObservation(observation: Observation): Promise<any> {
+        return fetch(this.apiUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${sessionStorage.getItem('auth_token')}`
+            },
+            body: JSON.stringify(observation),
+        })
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('Failed to post observation');
+            }
+            return response.json();
+        });
+    }
+
+}
+interface Observation {
+    observation_text: string;
+    longitude: number;
+    latitude: number;
+    photo_url: string;
+}
