2950ded28d905f839df0933df57f2a705b9b4618
TommexxL <tommexx_007@hotmail.com>
2026-01-12 15:27:26 +0100

    adding register to authentication service

--- front-end/src/app/services/authentication-service.ts
+++ front-end/src/app/services/authentication-service.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 export class AuthenticationService {
   
   private apiUrl = 'http://localhost:8000/api/login';
-
+  private registerUrl = 'http://localhost:8000/api/register';
   
 
   login(user: User): Promise<string> {
@@ -15,7 +15,10 @@ export class AuthenticationService {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ username: user.username, password: user.password, requiredRole: user.requiredRole}),
+      body: JSON.stringify({ 
+        username: user.username, 
+        password: user.password, 
+        requiredRole: user.requiredRole}),
     })
       .then((response) => {
         if (!response.ok) {
@@ -26,6 +29,29 @@ export class AuthenticationService {
       .then((data) => {
         sessionStorage.setItem('auth_token', data.token);
         return data;
+      });
+  }
+
+  register(user: RegisterUser): Promise<string> {
+    return fetch(this.registerUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: user.username,
+        email: user.email,
+        password: user.password,
+        password_confirmation: user.passwordConfirmation
+      }),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          return response.json().then(err => {
+            throw new Error(err.message || 'Registration failed');
+          });
+        }
+        return response.json();
       });
   }
 
@@ -44,3 +70,10 @@ interface User {
     password: string;
     requiredRole: string;
   }
+
+interface RegisterUser {
+  username: string;
+  email: string;
+  password: string;
+  passwordConfirmation: string;
+}
