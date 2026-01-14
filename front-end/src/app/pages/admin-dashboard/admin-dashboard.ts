375550b23b98c74fcf9b61586993e8839baf20af
Brt <barthunerbeincoding@gmail.com>
2026-01-14 13:34:48 +0100

    feat: add locations, users, and observations admin components to the admin dashboard

--- front-end/src/app/pages/admin-dashboard/admin-dashboard.ts
+++ front-end/src/app/pages/admin-dashboard/admin-dashboard.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { LocationsAdminComponent } from "../../components/locations-admin-component/locations-admin-component";
+import { UsersAdminComponent } from "../../components/users-admin-component/users-admin-component";
+import { ObservationsAdminComponent } from "../../components/observations-admin-component/observations-admin-component";
 
 @Component({
   selector: 'app-admin-dashboard',
-  imports: [],
+  imports: [LocationsAdminComponent, UsersAdminComponent, ObservationsAdminComponent],
   templateUrl: './admin-dashboard.html',
   styleUrl: './admin-dashboard.css',
 })
