4a9944fdb0ccf70b899621d615ee04e801d3eed2
Brt <barthunerbeincoding@gmail.com>
2026-01-15 10:44:54 +0100

    add simple table with observations, observation text is editable

--- front-end/src/app/components/observations-admin-component/observations-admin-component.ts
+++ front-end/src/app/components/observations-admin-component/observations-admin-component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { Observation, ObservationService } from '../../services/observation-service';
+import { LocationService } from '../../services/location-service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-observations-admin-component',
@@ -7,5 +10,24 @@ import { Component } from '@angular/core';
   styleUrl: './observations-admin-component.css',
 })
 export class ObservationsAdminComponent {
+
+  observationService = inject(ObservationService);
+
+  observations = signal<Observation[]>([]);
+  
+  tempInput: string = '';
+
+
+  ngOnInit() {
+    this.loadObservations();
+  }
+  async loadObservations() {
+    this.observationService.getObservations()
+    .then(obs => {console.log(obs)
+                  return obs;
+    } )
+    .then(obs => this.observations.set(obs))
+    .catch(error => console.error('Error loading observations:', error)); 
+  }
 
 }
