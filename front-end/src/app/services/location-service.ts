dba2f167df195f6ed64c05071ffc51a861a16bbf
Brt <barthunerbeincoding@gmail.com>
2026-01-15 15:16:54 +0100

    feat: implement updateLocation method in LocationService for location updates

--- front-end/src/app/services/location-service.ts
+++ front-end/src/app/services/location-service.ts
@@ -21,6 +21,24 @@ export class LocationService {
               return response.json();
           });
       }
+
+  updateLocation(id: number, data: Partial<Location>): Promise<Location> {
+      return fetch(`${this.apiUrl}/${id}`, {
+          method: 'PUT',
+          headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${sessionStorage.getItem('auth_token')}`
+          },
+          body: JSON.stringify(data)
+      })
+      .then((response) => {
+          if (!response.ok) {
+              throw new Error('Failed to update location');
+          }
+          return response.json();
+      });
+  }
+
 }
 
 export interface Location {
